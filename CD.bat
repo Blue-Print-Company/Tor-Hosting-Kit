5f9cec8b331fd54cdfe0cf4e89e79f79a4941cc2
OctonalXX <54666576+OctonalXX@users.noreply.github.com>
2021-05-16 11:45:25 +0800

    Update CD.bat

--- CD.bat
+++ CD.bat
@@ -2,7 +2,7 @@
 echo Tor Host Service Builder
 if [%1]==[] (set /p torver=Enter Tor Version:) else (set torver=%1)
 echo Deleting Old Builds...(Ignore Errors)
-rd /s /q .\Build
+rd /s /q .\Tor_Hosting_Kit
 rd /s /q .\Tor
 del tor.zip
 echo Done
@@ -11,12 +11,12 @@ curl https://dist.torproject.org/torbrowser/%torver%/tor-win32-0.4.5.7.zip --Out
 .\7z\7za e tor.zip -oTor
 mkdir Build
 dir
-xcopy /y /e .\Tor .\Build
-xcopy /y /e .\Files .\Build
+xcopy /y /e .\Tor .\Tor_Hosting_Kit
+xcopy /y /e .\Files .\Tor_Hosting_Kit
 echo Cleaning Up...
 rd /s /q .\Tor
 del tor.zip
-.\7z\7za a -tzip Build.zip .\Build -y
+.\7z\7za a -tzip Build.zip .\Tor_Hosting_Kit -y
 echo Complete
 
 set torver=
