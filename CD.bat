f2e353dc151f4306e0508f9eca71a0274bd7d391
OctonalXX <54666576+OctonalXX@users.noreply.github.com>
2021-08-11 14:26:52 +0800

    Update CD.bat

--- CD.bat
+++ CD.bat
@@ -4,7 +4,7 @@ set torver=%1
 del tor.zip
 echo Done
 echo Downloading Tor Release...
-curl https://dist.torproject.org/torbrowser/%torver%/tor-win32-0.4.5.7.zip --Output tor.zip
+curl https://dist.torproject.org/torbrowser/%torver%/tor-win32-0.4.5.9.zip --Output tor.zip
 .\7z\7za e tor.zip -oTor
 mkdir Tor_Hosting_Kit
 xcopy /y /e .\Tor .\Tor_Hosting_Kit\
